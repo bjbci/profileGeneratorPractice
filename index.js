99f185728c0e6cc536a6d09e11382bd875b865ab
Brian Bulowicz <bajebi.bb@gmail.com>
2022-12-30 21:42:30 -0600

    got prompt branch to work

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import inquirer from 'inquirer'
 
-
+let addEngineer
+let addIntern
+let managerName
+let managerID
+let managerEmail
+let officeNumber
+let engineerName
+let engineerID
+let engineerEmail
+let  engineerGithub
+let  internName
+let internID
+let internEmail
+let internSchool
 
 
 // const managerPrompt = () => {
@@ -10,22 +23,26 @@ import inquirer from 'inquirer'
     type: 'input',
     message: 'Enter the name of the team manager: ',
     name: 'managerName',
+    default:'le chat noir Big Boii',
    
   },
   {
     type: "input",
     message: "Enter employee ID: ",
-    name: "ID",
+    name: "managerID",
+    default:'007',
   },
   {
     type: "input",
     message: "Enter email address: ",
-    name: "email",
+    name: "managerEmail",
+    default:'ihatethisjob@work.com',
   },
   {
     type: "input",
     message: "Enter the office number of the team manager",
     name: "officeNumber",
+    default:'1313',
   },
   {
     type: "list",
@@ -33,169 +50,301 @@ import inquirer from 'inquirer'
       message: "Would you like to add an engineer, add an intern or finish",
       choices: ["Add Engineer","Add Intern","Finish"],
   },
-    //   default: true
-    // }]).then(answers => {
-    // //   let {empName, empID, email, githubName,school, addAnother} = employeeData
-    // //   let employee;
-    //   if(separator === "Engineer") {
-    //     employee = new Engineer(engineerName, engineerID, engineerEmail, engineerGithub)
-    //     console.log(employee)
-    //   } else if(separator === "Intern") {
-    //     employee = new Intern(internName,internID,internEmail,internSchool)
-    //     console.log(employee)
-    //   }else {
-
-    //   }
-    //   },
-//   {
-//     type: "choices",
-//     message: "Would you like to add an engineer, add an intern or finish",
-//     name: "separator",
-//     choices: ["Add Engineer", new inquirer.Separator(),"Add Intern",new inquirer.Separator(), "Finish"]
-//   },
-  {
-    type: 'input',
-    message: 'Enter the name of the Team Engineer: ',
-    name: 'engineerName',
-    when: (input) => input.positionSelection === "Add Engineer"
-   
-  },
-  {
-    type: "input",
-    message: "Enter Engineer employee ID: ",
-    name: "engineerID",
-    when: (input) => input.positionSelection === "Add Engineer"
-  },
-  {
-    type: "input",
-    message: "Enter Engineer email address: ",
-    name: "engineerEmail",
-    when: (input) => input.positionSelection === "Add Engineer"
-  },
-  {
-    type: "input",
-    message: "Enter Engineer Github Username",
-    name: "engineerGithub",
-    when: (input) => input.positionSelection === "Add Engineer"
-  },
-  {
-    type: "input",
-    message: "Enter Intern name",
-    name: "internName",
-    when: (input) => input.positionSelection === "Add Intern"
-  },
-  {
-    type: "input",
-    message: "Enter Intern ID",
-    name: "internID",
-    when: (input) => input.positionSelection === "Add Intern"
-  },
-  {
-    type: "input",
-    message: "Enter Intern email",
-    name: "internEmail",
-    when: (input) => input.positionSelection === "Add Intern"
-  },
-  {
-    type: "input",
-    message: "Enter the school you attend: ",
-    name: "internSchool",
-    when:(input)=> input.positionSelection==="Add Intern"
-  },
-])
-  .then(answers=>{
+]).then(answers=>{
     console.log(answers)
-  }
-)
-// ]).then(managerData => {
-//     const {managerName, ID, email, officeNumber} = managerData;
-//     const manager = new Manager(managerName, ID, email, officeNumber);
-//     team.push(manager)
-//     console.log(manager)
-//   })
-//   };
-  
-//   const employeePrompt = () => {
-//     return inquirer.prompt([{
-//       type: "list",
-//       message: "Select your position",
-//       name: "positionSelection",
-//       choices: ["Engineer", "Intern", "Finish"],
-//     },
+    console.log(answers.separator)
+        if (answers.separator==="Add Engineer"){   //"Add Engineer"
+            inquirer.prompt([
+                  {
+                type: 'input',
+                message: 'Enter the name of the Team Engineer: ',
+                name: 'engineerName',
+                default:'Bucky the golden lab',
+                // when: (input) => input.positionSelection === "Add Engineer"
+               },
+              {
+                type: "input",
+                message: "Enter Engineer employee ID: ",
+                name: "engineerID",
+                default:'5643',
+                // when: (input) => input.positionSelection === "Add Engineer"
+              },
+              {
+                type: "input",
+                message: "Enter Engineer email address: ",
+                name: "engineerEmail",
+                default:'iamasmartypants@work.com',
+                // when: (input) => input.positionSelection === "Add Engineer"
+              },
+              {
+                type: "input",
+                message: "Enter Engineer Github Username",
+                name: "engineerGithub",
+                default:'github?, whats that?',
+                // when: (input) => input.positionSelection === "Add Engineer"
+              },
+            ])
+        }else if(answers.separator==="Add Intern"){
+            inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter Intern name",
+                name: "internName",
+                default:'Freebie the bunny',
+                // when: (input) => input.positionSelection === "Add Intern"
+              },
+              {
+                type: "input",
+                message: "Enter Intern ID",
+                name: "internID",
+                default:'7',
+                // when: (input) => input.positionSelection === "Add Intern"
+              },
+              {
+                type: "input",
+                message: "Enter Intern email",
+                name: "internEmail",
+                default:'imnewheredontfireme@work.com',
+                // when: (input) => input.positionSelection === "Add Intern"
+              },
+              {
+                type: "input",
+                message: "Enter the school you attend: ",
+                name: "internSchool",
+                default:'dont YOU know where I went to school?',
+                // when:(input)=> input.positionSelection==="Add Intern"
+              },
+            ])
+            }else{
+                if(answers.separator==="Finish")
+                inquirer.prompt([
+                {
+                    type:"confirm",
+                    message:"Are you sure you want to complete your team?",
+                    name: "internSchool",
+                    default:'do YOU know where I went to school?',
+                },
+            ])
+
+            }
+    })
+    
+    // then(answers=>{
+    //     if(answers.separator===answers.addIntern){
+    //         inquirer.prompt([
+    //         {
+    //             type: "input",
+    //             message: "Enter Intern name",
+    //             name: "internName",
+    //             default:'Freebie the bunny',
+    //             // when: (input) => input.positionSelection === "Add Intern"
+    //           },
+    //           {
+    //             type: "input",
+    //             message: "Enter Intern ID",
+    //             name: "internID",
+    //             default:'7',
+    //             // when: (input) => input.positionSelection === "Add Intern"
+    //           },
+    //           {
+    //             type: "input",
+    //             message: "Enter Intern email",
+    //             name: "internEmail",
+    //             default:'imnewheredontfireme@work.com',
+    //             // when: (input) => input.positionSelection === "Add Intern"
+    //           },
+    //           {
+    //             type: "input",
+    //             message: "Enter the school you attend: ",
+    //             name: "internSchool",
+    //             default:'do YOU know where I went to school?',
+    //             // when:(input)=> input.positionSelection==="Add Intern"
+    //           },
+    //         ])
+    //         }
+    //     }).then(answers=>{
+    //     if(prompt.separator===answers.finish)
+    //         inquirer.prompt([
+    //         {
+    //             type:"confirm",
+    //             message:"Are you sure you want to complete your team?",
+    //             name: "internSchool",
+    //             default:'do YOU know where I went to school?',
+    //         },
+    //     ])
+    // })
+ ////////////////////////       /////////////////////
+//      }   
+//     }
+// ]).then(answers=>{
+//      addEngineer=answers.addEngineer
+//      addIntern=answers.addIntern
+//      managerName
+//      managerID
+//      managerEmail
+//      officeNumber
+//      engineerName
+//      engineerID
+//      engineerEmail
+//      engineerGithub
+//      internName
+//      internID
+//      internEmail
+//      internSchool
+
+
+// })
+//////////////////////////////////////////////////////////
+
+// import inquirer from 'inquirer'
+
+// let addEngineer
+// let addIntern
+// let managerName
+// let managerID
+// let managerEmail
+// let officeNumber
+// let engineerName
+// let engineerID
+// let engineerEmail
+// let  engineerGithub
+// let  internName
+// let internID
+// let internEmail
+// let internSchool
+
+
+// // const managerPrompt = () => {
+// //     return inquirer.prompt([
+//     inquirer.prompt([
 //   {
-//       type: 'input',
-//       message: 'Enter the name of this Employee: ',
-//       name: 'empName',
+//     type: 'input',
+//     message: 'Enter the name of the team manager: ',
+//     name: 'managerName',
+//     default:'le chat noir Big Boii',
+   
 //   },
 //   {
-    
-//       type: "input",
-//       message: "Enter employee ID: ",
-//       name: "empID", 
+//     type: "input",
+//     message: "Enter employee ID: ",
+//     name: "managerID",
+//     default:'007',
 //   },
 //   {
 //     type: "input",
 //     message: "Enter email address: ",
-//     name: "email",
+//     name: "managerEmail",
+//     default:'ihatethisjob@work.com',
 //   },
 //   {
 //     type: "input",
-//     message: "Enter Engineer Github Username",
-//     name: "githubName",
-//     when: (input) => input.positionSelection === "Engineer"
+//     message: "Enter the office number of the team manager",
+//     name: "officeNumber",
+//     default:'1313',
 //   },
 //   {
-//     type: "input",
-//     message: "Enter the school you attend: ",
-//     name: "school",
-//     when:(input)=> input.positionSelection==="Intern"
-//   },
-//   {
-//     type: "confirm",
-//     name: "finish",
-//     message: "Would you like to finish your team",
-//     default: true
-//   }]).then(employeeData => {
-//     let {empName, empID, email, githubName,school, addAnother} = employeeData
-//     let employee;
-//     if(positionSelection === "Engineer") {
-//       employee = new Engineer(empName, empID, email, githubName)
-//       console.log(employee)
-//     } else if(positionSelection === "Intern") {
-//       employee = new Intern(empName, empID, email, school)
-//       console.log(employee)
+//     type: "list",
+//       name: "separator",
+//       message: "Would you like to add an engineer, add an intern or finish",
+//       choices: ["Add Engineer","Add Intern","Finish"],
+//       validate:(answer)=>{
+//         if (answer===answers.addEngineer){   //"Add Engineer"
+//             inquirer.prompt([
+//                   {
+//                 type: 'input',
+//                 message: 'Enter the name of the Team Engineer: ',
+//                 name: 'engineerName',
+//                 default:'Bucky the golden lab',
+//                 // when: (input) => input.positionSelection === "Add Engineer"
+//                },
+//               {
+//                 type: "input",
+//                 message: "Enter Engineer employee ID: ",
+//                 name: "engineerID",
+//                 default:'5643',
+//                 // when: (input) => input.positionSelection === "Add Engineer"
+//               },
+//               {
+//                 type: "input",
+//                 message: "Enter Engineer email address: ",
+//                 name: "engineerEmail",
+//                 default:'iamasmartypants@work.com',
+//                 // when: (input) => input.positionSelection === "Add Engineer"
+//               },
+//               {
+//                 type: "input",
+//                 message: "Enter Engineer Github Username",
+//                 name: "engineerGithub",
+//                 default:'github?, whats that?',
+//                 // when: (input) => input.positionSelection === "Add Engineer"
+//               },
+//             ])
+//         }else if(answer==="Add Intern"){
+//             inquirer.prompt([
+//             {
+//                 type: "input",
+//                 message: "Enter Intern name",
+//                 name: "internName",
+//                 default:'Freebie the bunny',
+//                 // when: (input) => input.positionSelection === "Add Intern"
+//               },
+//               {
+//                 type: "input",
+//                 message: "Enter Intern ID",
+//                 name: "internID",
+//                 default:'7',
+//                 // when: (input) => input.positionSelection === "Add Intern"
+//               },
+//               {
+//                 type: "input",
+//                 message: "Enter Intern email",
+//                 name: "internEmail",
+//                 default:'imnewheredontfireme@work.com',
+//                 // when: (input) => input.positionSelection === "Add Intern"
+//               },
+//               {
+//                 type: "input",
+//                 message: "Enter the school you attend: ",
+//                 name: "internSchool",
+//                 default:'do YOU know where I went to school?',
+//                 // when:(input)=> input.positionSelection==="Add Intern"
+//               },
+//             ])
+//         }else{
+//             inquirer.prompt([
+//             {
+//                 type:"confirm",
+//                 message:"Are you sure you want to complete your team?",
+//                 name: "internSchool",
+//                 default:'do YOU know where I went to school?',
+//             },
+//         ])
+//         }
+//      }   
 //     }
-//     team.push(employee)
-//     if(addAnother) {
-//       return employeePrompt(team)
-//     } else {
-//       return team
-//     }
-//   })
-//   }
-  
-//   managerPrompt()
-//   .then(employeePrompt)
-  
-  
-  
-//   const questions = [
-   
-//     {
-//       type: "input",
-//       message: "Enter your name: ",
-//       name: "engineerInternName",
-//     },
-//     {
-//       type: "input",
-//       message: "Enter your Github Username",
-//       name: "githubName",
-//     },
-//     {
-//       type: "input",
-//       message: "Enter the school you attend: ",
-//       name: "school",
-//       when:(input)=> input.school==="Intern"
-//     },
-//   ]
+// ]).then(answers=>{
+//      addEngineer=answers.addEngineer
+//      addIntern=answers.addIntern
+//      managerName
+//      managerID
+//      managerEmail
+//      officeNumber
+//      engineerName
+//      engineerID
+//      engineerEmail
+//      engineerGithub
+//      internName
+//      internID
+//      internEmail
+//      internSchool
+
+
+// })
+        
+    
+
+
+
+
+    
   
